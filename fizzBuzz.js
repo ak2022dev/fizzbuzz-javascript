5b37fdf289dcfb4bd8fe5293f715dc5ec3c290cc
Amir Khan <ak2022dev@gmail.com>
2022-06-25 19:24:13 +0100

    Second failing test passed minimally

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -4,6 +4,9 @@ const fizzBuzz = (num) => {
   if ((num%3)===0) {
     return 'Fizz';
   }
+  if (num===5) {
+    return 'Buzz';
+  }
 };
 
 module.exports = fizzBuzz;
--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -6,5 +6,8 @@ describe( 'fizzBuzz', () => {
   it( 'returns "Fizz" given input  3', () => {
     expect(fizzBuzz(3)).toBe('Fizz');
   });
+  it( 'returns "Buzz" given input 5', () => {
+    expect(fizzBuzz(5)).toBe('Buzz');
+  });
 });
 
